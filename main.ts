bf9e639a0636e1f49161e7cb29bd8f913b7cc608
MundGcalliope <georgfuerst@gmail.com>
2025-07-12 09:53:25 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,6 @@
+basic.showString("" + (control.deviceSerialNumber()))
+basic.forever(function () {
+    basic.showString("\"CO2\"" + "Welt")
+    basic.showString("hi!")
+    basic.showString("hi!")
+})
